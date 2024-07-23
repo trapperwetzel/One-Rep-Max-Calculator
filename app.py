76d8dd717f51b69b18c406d534cf8beac77561c5
trapperwetzel <trapperwetzel@gmail.com>
2024-07-22 22:55:53 -0500

    Contact Page Updated

--- app.py
+++ app.py
@@ -26,10 +26,16 @@ def index():
 def about():
     return render_template('about.html') 
 
-@app.route('/contact') 
-def contact():
-    return render_template('contact.html') 
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        
+        return render_template('index.html')
+    return render_template('contact.html')
 
 
 
